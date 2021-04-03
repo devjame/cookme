f9a7f88c4f1b041787bd37a532ff53fa134890c6
devjames <devjames79@gmail.com>
2021-04-03 23:49:10 +0100

    add favorited recipes

--- src/pages/saved.vue
+++ src/pages/saved.vue
@@ -1,11 +1,22 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page>
+     <recipe-cart :recipes="favorited">
+       Favorited
+     </recipe-cart>
   </q-page>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import recipeCart from '../components/recipeCart.vue';
+
 export default {
   // name: 'PageName',
+  components: { recipeCart },
+  computed: {
+    ...mapState({
+      favorited: (state) => state.recipe.favorited,
+    }),
+  },
 };
 </script>
